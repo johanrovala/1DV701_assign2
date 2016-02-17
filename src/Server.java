303c28de1542b15517f71758527ada8231704082
Jakob <193brandt@gmail.com>
2016-02-17 11:08:34 +0100

    Added readcontent from file method in IOHelper

--- src/Server.java
+++ src/Server.java
@@ -5,16 +5,18 @@ import java.net.Socket;
 /**
  * Created by johanrovala on 15/02/16.
  */
-public class Server {
-
+public class Server
+{
     private static ServerSocket serverSocket;
     private static final int PORT = 8888;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {
 
         serverSocket = new ServerSocket(PORT);
 
-        while (true){
+        while (true)
+        {
             Socket socket = serverSocket.accept();
             new GetReqThread(socket).start();
         }
