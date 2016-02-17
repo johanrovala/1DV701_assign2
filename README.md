fe395f93213dbae722c174664694e524b4ef7b84
johanrovala <jr.johan.ro@gmail.com>
2016-02-17 11:47:55 +0100

    Uppdaterad readme och påbörjad Client klass

---
+++ README.md
@@ -0,0 +1,4 @@
+
+- 1 Requestlines, ifall vi får fler än en. Detta problem gäller enbart när html/index.html
+    Hårdkodad lösning på hur vi får upp både htmltext + bilder när vi skriver in html/        index.html
+
--- src/GetReqThread.java
+++ src/GetReqThread.java
@@ -11,20 +11,14 @@ import java.nio.file.Paths;
  */
 public class GetReqThread extends Thread{
 
-    Socket client;
-    PrintWriter outPut;
-    DataInputStream dataInputStream;
+    Client client;
     byte[] buf = new byte[100];
-    CharSequence cs1 = "GET";
-    private String methodRequest;
     private HTTPRequest httpRequest;
     private IOHelper ioHelper;
     // datainputstream för att hämta get req?
 
     public GetReqThread(Socket s) throws IOException {
-        client = s;
-        outPut = new PrintWriter(client.getOutputStream());
-        dataInputStream = new DataInputStream(client.getInputStream());
+        client = new Client(s);
         ioHelper = new IOHelper();
     }
 
@@ -43,6 +37,16 @@ public class GetReqThread extends Thread{
         System.out.println("Path " + httpRequest.getPath());
         System.out.println("Options " +httpRequest.getOptions());
         System.out.println(ioHelper.fileExist(httpRequest.getPath()));
+
+       // File file = new File("src"+httpRequest.getPath());
+       /* try {
+            dataInputStream.read(buf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String req1 = new String(buf);
+        httpRequest = new HTTPRequest(req1);
+        System.out.println("Path " + httpRequest.getPath());*/
        // File file = new File("src/main.html/index.main.html");
        // System.out.println("runs");
 
@@ -67,11 +71,5 @@ public class GetReqThread extends Thread{
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-
-    public String readFile(String path, Charset encoding) throws IOException {
-        byte[] content = Files.readAllBytes(Paths.get(path));
-        return new String(content, encoding);
     }
 }
